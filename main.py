3059909a5d6ae4573df5f13f93d44c000c2b7751
fylhsq1 <andrijbikov134@gmail.com>
2022-04-03 18:10:16 +0300

    initial commit

--- main.py
+++ main.py
@@ -31,22 +31,48 @@ class Human:
     def is_alive(self):
         pass
     def live(self, dey):
+        pass
 class Auto:
-    def __init__(self,brand):
-        self.brand=brand
-        self.passengers=[]
-    def add_passengers(self,human):
-        self.passengers.append(human)
-    def print_passengers_names(self):
-        if self.passengers!=[]:
-            print(f"Names of{self.brand}passengers:")
-            for passengers in self.passengers:
-                print(passengers.name)
+    def __init__(self,brand_list):
+        self.brand=random.choice(list(brand_list))
+        self.fuel=brand_list[self.brand]["fuel"]
+        self.strength=brand_list[self.brand]["strength"]
+        self.consumption=brand_list[self.brand]["consumption"]
+    brands_of_car={"Toyota Crown S170 Royal Saloon":{"fuel":70, "consumption":12.2, "strength":100},
+                   "BMW E38 750il 95":{"fuel":95, "consumption":15.2, "strength":326},
+                   "Lexus":{"fuel":70, "consumption":10.9, "strength":264},
+                   "Mersedes":{"fuel":62, "consumption":8.7, "strength":150}}
+    def drive(self):
+        if self.strength>0 and self.fuel>=self.consumption:
+            self.fuel-=self.consumption
+            self.strength-=1
+            return True
         else:
-            print(f"There are passengers in {self.brand}")
-anna=Human("Anna")
-vika=Human("Vika")
-car=Auto("Toyota")
-car.add_passengers(anna)
-car.add_passengers(vika)
-car.print_passengers_names()
+            print("The car can not move")
+            return False
+class House:
+    def __init__(self):
+        self.mess=0
+        self.food=0
+class Job:
+    def __init__(self, job_list):
+        self.job=random.choice(list(job_list))
+        self.salary=job_list[self.job]["salary"]
+        self.gladness_lass=job_list[self.job]["gladness_lass"]
+    job_list={
+        "lava developer":{"salary": 50, "gladness_lass":10},
+    "Python developer":{"salary": 40, "gladness_lass":3},
+        "C++ developer":{"salary": 45, "gladness_lass":25},
+        "Rust developer":{"salary": 70, "gladness_lass":1},
+        "C# developer":{"salary": 90, "gladness_lass":2}}
+    def get_home(self):
+        self.home=House()
+    def get_car(self):
+        self.car=Auto(brand_of_car)
+    def get_fob(self):
+        if self.car.drive():
+            pass
+        else:
+            self.to_repair()
+            return
+        self.job=Job(job_list)
